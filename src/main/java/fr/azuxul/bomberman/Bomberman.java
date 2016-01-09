b0373b84cee37a3714a56be42e93462c935284cb
azuxul <contact@azuxul.fr>
2016-01-09 01:01:30 +0100

    Initial commit First commit of 2016 !!

---
+++ src/main/java/fr/azuxul/bomberman/Bomberman.java
@@ -0,0 +1,17 @@
+package fr.azuxul.bomberman;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Main class of Bomberman plugin
+ *
+ * @author Azuxul
+ * @version 1.0
+ */
+public class Bomberman extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
