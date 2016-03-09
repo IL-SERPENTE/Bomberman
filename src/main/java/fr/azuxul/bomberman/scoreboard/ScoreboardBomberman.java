c7a24801ba254bef3277d495f0f204e9684ff439
azuxul <contact@azuxul.fr>
2016-03-09 17:20:57 +0100

    Update scoreboard

--- src/main/java/fr/azuxul/bomberman/scoreboard/ScoreboardBomberman.java
+++ src/main/java/fr/azuxul/bomberman/scoreboard/ScoreboardBomberman.java
@@ -30,14 +30,15 @@ public class ScoreboardBomberman {
         ObjectiveSign objectiveSign = new ObjectiveSign("BombermanSc", ChatColor.AQUA + gameManager.getGameName());
 
         objectiveSign.setLine(9, " ");
-        objectiveSign.setLine(8, ChatColor.GOLD + "⚡  Vitesse :" + ChatColor.RESET + " -1");
-        objectiveSign.setLine(7, ChatColor.GOLD + "⠾   Nombre de bombes :" + ChatColor.RESET + " -1");
-        objectiveSign.setLine(6, ChatColor.GOLD + "⚠  Portée de l’explosion :" + ChatColor.RESET + " -1");
+        objectiveSign.setLine(8, ChatColor.GOLD + "\u26A1  Vitesse :" + ChatColor.RESET + " -1");
+        objectiveSign.setLine(7, ChatColor.GOLD + "\u283E   Nombre de bombes :" + ChatColor.RESET + " -1");
+        objectiveSign.setLine(6, ChatColor.GOLD + "\u26A0  Portée de l’explosion :" + ChatColor.RESET + " -1");
         objectiveSign.setLine(5, "  ");
-        objectiveSign.setLine(4, ChatColor.BLACK.toString() + ChatColor.BOLD + "▓▒░ BOOSTER ACTIF ░▒▓");
-        objectiveSign.setLine(3, ChatColor.RESET + "Aucun");
+        objectiveSign.setLine(4, ChatColor.GRAY.toString() + ChatColor.BOLD + "\u2593\u2592\u2593 BOOSTER ACTIF \u2593\u2592\u2593");
+        objectiveSign.setLine(3, ChatColor.RESET + "  -> Aucun");
         objectiveSign.setLine(2, "   ");
-        objectiveSign.setLine(1, ChatColor.GOLD + "☺  Joueurs restant :" + ChatColor.RESET + " -1");
+        objectiveSign.setLine(1, ChatColor.GOLD + "\u263A  Joueurs restant :" + ChatColor.RESET + " -1");
+        objectiveSign.setLine(0, ChatColor.GOLD + "\u231B   Temps restant : " + ChatColor.RESET + "00:00");
 
         return objectiveSign;
     }
@@ -58,19 +59,19 @@ public class ScoreboardBomberman {
         }
 
         try {
-            objectiveSign.setLine(0, ChatColor.GOLD + "⌛   Temps restant : " + ChatColor.RESET + String.format("%02d:%02d", gameManager.getTimer().getMinutes(), gameManager.getTimer().getSeconds()));
+            objectiveSign.setLine(0, ChatColor.GOLD + "\u231B   Temps restant : " + ChatColor.RESET + String.format("%02d:%02d", gameManager.getTimer().getMinutes(), gameManager.getTimer().getSeconds()));
         } catch (Exception e) {
             gameManager.getServer().getLogger().info(String.valueOf(e));
         }
 
         PowerupTypes powerup = playerBomberman.getPowerupTypes();
 
-        objectiveSign.setLine(3, ChatColor.RESET + (powerup == null ? "Aucun" : powerup.getName()));
-        objectiveSign.setLine(8, ChatColor.GOLD + "⚡   Vitesse : " + ChatColor.RESET + (Math.round(playerBomberman.getSpeed() * 10) - 2));
-        objectiveSign.setLine(7, ChatColor.GOLD + "⠾   Nombre de bombes : " + ChatColor.RESET + playerBomberman.getBombNumber());
-        objectiveSign.setLine(6, ChatColor.GOLD + "⚠  Portée de l’explosion : " + ChatColor.RESET + playerBomberman.getRadius());
+        objectiveSign.setLine(3, ChatColor.RESET + "  -> " + (powerup == null ? "Aucun" : powerup.getName()));
+        objectiveSign.setLine(8, ChatColor.GOLD + "\u26A1   Vitesse : " + ChatColor.RESET + (Math.round(playerBomberman.getSpeed() * 10) - 2));
+        objectiveSign.setLine(7, ChatColor.GOLD + "\u283E   Nombre de bombes : " + ChatColor.RESET + playerBomberman.getBombNumber());
+        objectiveSign.setLine(6, ChatColor.GOLD + "\u26A0  Portée de l’explosion : " + ChatColor.RESET + playerBomberman.getRadius());
 
-        objectiveSign.setLine(1, ChatColor.GOLD + "☺   Joueurs restant : " + ChatColor.RESET + gameManager.getConnectedPlayers());
+        objectiveSign.setLine(1, ChatColor.GOLD + "\u263A   Joueurs restant : " + ChatColor.RESET + gameManager.getConnectedPlayers());
 
         if (!player.getGameMode().equals(GameMode.SPECTATOR) && powerup != null)
             ActionBarAPI.sendMessage(player, ChatColor.GREEN + "Booster : " + ChatColor.GOLD + powerup.getName());
