063f1fa2237920ec6ce1b20c99cacd17e4eec7d1
BlueSlime <jeremylevilain78@orange.fr>
2017-08-23 19:41:15 +0200

    Publishing source code.

--- src/main/java/net/samagames/bomberman/player/PlayerBomberman.java
+++ src/main/java/net/samagames/bomberman/player/PlayerBomberman.java
@@ -23,11 +23,21 @@ import org.bukkit.util.Vector;
 import java.util.*;
 import java.util.stream.Collectors;
 
-/**
- * Player for Bomberman plugin
+/*
+ * This file is part of Bomberman.
  *
- * @author Azuxul
- * @version 1.0
+ * Bomberman is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bomberman is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bomberman.  If not, see <http://www.gnu.org/licenses/>.
  */
 public class PlayerBomberman extends GamePlayer {
 
--- src/main/java/net/samagames/bomberman/powerup/PowerupManager.java
+++ src/main/java/net/samagames/bomberman/powerup/PowerupManager.java
@@ -8,10 +8,21 @@ import org.bukkit.craftbukkit.v1_9_R2.CraftWorld;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Powerup manager
+/*
+ * This file is part of Bomberman.
  *
- * @author Azuxul
+ * Bomberman is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bomberman is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bomberman.  If not, see <http://www.gnu.org/licenses/>.
  */
 public class PowerupManager {
 
